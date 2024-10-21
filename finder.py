a8b81ad78e830f45364e5eb26bcac5cded755713
Dark Society <darksociety077@gmail.com>
2024-10-21 11:09:05 -0400

    Add files via upload

---
+++ finder.py
@@ -0,0 +1,41 @@
+from colorama import Fore
+import requests
+import sys
+import os
+
+
+# dark_society07
+
+def site():
+    os.system('clear')
+    search = open("page.txt", 'r')
+    url = input(Fore.LIGHTGREEN_EX+"""┌─[Dark-Society07~@HOME]
+ └──╼ $  Entr URL :""")
+
+    if "http://" or "https://" not in url:
+        url = "http://"+url
+    else:
+        pass
+    try:
+        for page in search:
+            req = requests.get(url + "/" + page)
+            if req.status_code == 200:
+                print(Fore.LIGHTGREEN_EX + "[+] " + Fore.LIGHTWHITE_EX + "page ok " + url + "/" + page)
+            else:
+            	pass
+                #print(Fore.LIGHTRED_EX + "[-] " + Fore.LIGHTWHITE_EX + "not page " + url + "/" + page)
+    except requests.exceptions.MissingSchema:
+        print("\nUrl eshtebah ast lotfan dobare vard konid! : \n")
+    except NameResolutionError:
+    	os.system("python3 dibagsite.py")
+    except NameError: 
+    	os.system("python3 dibagsite.py")
+    	print("place Enter URL...")
+    	
+    	
+    	
+    	
+    	
+    	
+site()
+
